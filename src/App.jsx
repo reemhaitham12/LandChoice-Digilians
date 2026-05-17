7945708f562ba98a059fb15bb97e21dc8e887409
reemhaitham <reemhaitham69@gmail.com>
2026-05-17 14:01:11 +0300

    Fix App routes after merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import { Routes, Route } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-import Explore from './pages/Explore';
+import Layout from './Components/Layout';
+import { Home } from './Pages/Home';
+import { Explore } from './Pages/Explore';
+import { SalaryFit } from './Pages/SalaryFit';
+import { ComparyCountry } from './Pages/CompareCountry';
+import { Checklist } from './Pages/CheckList';
+import { News } from './Pages/News';
+import { CountryDetails } from './Pages/CountryDetails';
+import { NotFound } from './Pages/NotFound';
 
-const App = () => {
-  return (
-    <div className="min-h-screen bg-dark-900">
-      {/* <Navbar /> */}
-      <Routes>
-        <Route path="/" element={<Explore />} />
-        <Route path="/explore" element={<Explore />} />
-      </Routes>
-    </div>
-  );
-};
+const routers = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+
+      { path: 'explore', element: <Explore /> },
+      { path: 'salary-fit', element: <SalaryFit /> },
+      { path: 'compare', element: <ComparyCountry /> },
+      { path: 'checklist', element: <Checklist /> },
+      { path: 'news', element: <News/> },
+      { path: 'country/:id', element: <CountryDetails /> },
+      { path: '*', element: <NotFound/> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={routers} />;
+}
 
 export default App;
